69d3d35ef63e9f6c5668d50965bdd8ffcfa6c528
deekshith <deekshith@Mac-mini.local>
2023-02-07 18:58:01 +0530

    stage-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../data";
 
 function App() {
   return (
     <div className="App">
       <Header />
-
-      <Note />
+      {notes.map((note) => (
+        <Note title={note.title} content={note.content} />
+      ))}
       <Footer />
     </div>
   );
